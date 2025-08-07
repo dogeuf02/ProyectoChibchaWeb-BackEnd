d8803be36b9125788e660ede496ef2a2634050ae
AndresPescador <carlosandrespescador8@gmail.com>
2025-08-07 17:33:33 -0500

    Update create method for medioPago

--- src/main/java/com/debloopers/chibchaweb/controller/MedioPagoController.java
+++ src/main/java/com/debloopers/chibchaweb/controller/MedioPagoController.java
@@ -8,6 +8,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +30,7 @@ public class MedioPagoController {
 
     private final MedioPagoService medioPagoService;
 
+    @Autowired
     public MedioPagoController(final MedioPagoService medioPagoService) {
         this.medioPagoService = medioPagoService;
     }
